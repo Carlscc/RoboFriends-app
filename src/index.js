3a88bed4c29c0461b31f7d78dccd863df75bfb7d
Caroline Chan <38971399+Carlscc@users.noreply.github.com>
2019-01-05 10:28:43 +0000

    Adding files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App.js';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './containers/Robots.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
